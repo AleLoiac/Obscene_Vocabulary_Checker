8e3defbe3af2cbeafe2b8ed31b2baca5346d1700
AleLoiac <alessandroloiac@gmail.com>
2023-01-31 12:26:52 +0100

    31/01

--- Main.go
+++ Main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var fileName string
+
 func main() {
+
+	fmt.Scan(&fileName)
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file) // create a new Scanner for the file
+
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // the Text() function converts the scanned bytes to a string
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
 }

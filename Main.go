e8633b6e9f67503352391433f669fd653be7455c
AleLoiac <alessandroloiac@gmail.com>
2023-01-31 15:08:31 +0100

    31/01

--- Main.go
+++ Main.go
@@ -5,33 +5,66 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 var fileName string
+var word string
 
-func main() {
-
-	_, err := fmt.Scan(&fileName)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+func printWords(file *os.File) { //prints all names in the file on a different row
 
 	scanner := bufio.NewScanner(file) // create a new Scanner for the file
 
 	scanner.Split(bufio.ScanWords)
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // the Text() function converts the scanned bytes to a string
+		fmt.Println(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
 
+func readFile(fileName string) *os.File {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+func checkWord(file *os.File) {
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanWords)
+
+	x := ""
+	for scanner.Scan() {
+		if strings.ToLower(scanner.Text()) == strings.ToLower(word) {
+			x = "True"
+			break
+		} else {
+			x = "False"
+		}
+	}
+	fmt.Println(x)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+
+	_, err := fmt.Scan(&fileName, &word)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	file := readFile(fileName)
+	defer file.Close() //defer the closure
+
+	checkWord(file)
 }
